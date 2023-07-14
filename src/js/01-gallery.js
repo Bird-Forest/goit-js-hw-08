2ab17ef94b721a552eea2238ef5c496206b1bacf
Bird-Forest <bird.forest70@gmail.com>
2023-07-14 14:04:04 +0300

    add task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const pictures = document.querySelector('.gallery');
+
+const markup = galleryItems.map((elem) =>
+`<li class="gallery__item">
+   <a class="gallery__link" href="${elem.original}">
+      <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+   </a>
+</li>
+`).join('');
+
+pictures.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const formUser = document.querySelector('.feedback-form')
+console.dir(formUser);
+formUser.addEventListener('submit', throttle(onSubmit, 500));
+
+function updatePage() {
+  localStorage.removeItem("feedback-form-state");
+};
+updatePage();
+
+function onSubmit(evt) {
+  evt.preventDefault()
+  const { email, message } = evt.currentTarget.elements
+  user = {
+        email: email.value,
+        message: message.value,
+        }
+  console.dir(user);
+  
+  localStorage.setItem("feedback-form-state", JSON.stringify(user));
+  
+  formUser.reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function updatePage() {
+//   if (localStorage.getItem("feedback-form-state") ?? {}) {
+//     return
+//   }
+  
+//   localStorage.getItem("feedback-form-state", JSON.parse(user));
+//   console.dir(localStorage.getItem("feedback-form-state", JSON.parse(user)))
+//    localStorage.removeItem("feedback-form-state");
+// }
